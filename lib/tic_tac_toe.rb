4036cd64820f3c2656b78f5991ad3706bdf6e904
Andrew Hyde <andrewryanhyde@hotmail.co.uk>
2018-05-30 13:14:30 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -39,3 +39,19 @@ def valid_move?(board, index)
   end
 end
 
+def position_taken(board, index)
+  if board[index] == " " || board[index] == "" || board[index] == nil
+    return false
+  else
+    return true
+  end
+end
+
+def on_board(int)
+    if int.between?(0,8) == true
+      return true
+    else 
+      return false
+    end
+end
+
